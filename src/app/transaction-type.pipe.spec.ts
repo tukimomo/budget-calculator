35c58d4878b8427604bbe3b46952d69c390d5bef
Trân Võ <vobao.tran2612@gmail.com>
2024-02-15 14:41:38 +0700

    add test for transaction type pipe

--- src/app/transaction-type.pipe.spec.ts
+++ src/app/transaction-type.pipe.spec.ts
@@ -1,8 +1,17 @@
+import { TransactionType } from './models/transaction';
 import { TransactionTypePipe } from './transaction-type.pipe';
 
 describe('TransactionTypePipe', () => {
   it('create an instance', () => {
     const pipe = new TransactionTypePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should return transaction type expense when passing in expense string', () => {
+    expect(new TransactionTypePipe().transform("EXPENSE")).toBe(TransactionType.EXPENSE);
+  });
+
+  it('should return transaction type expense when passing in income string', () => {
+    expect(new TransactionTypePipe().transform("INCOME")).toBe(TransactionType.INCOME);
   });
 });
